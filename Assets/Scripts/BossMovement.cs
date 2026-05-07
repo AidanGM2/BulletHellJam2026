60680b009c8573f9b81391adadfdc308ea04c1c8
AidanGM2 <agmellon@gmail.com>
2026-05-07 00:10:35 -0500

    Started working on boss movement

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+
+    public float bossMoveSpeed = 5f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(bossMoveSpeed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2( -bossMoveSpeed, 0);
+        }
+
+        if(Vector2.Distance(currentPoint.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+            Debug.Log("Made it to point B");
+        }
+        if (Vector2.Distance(currentPoint.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+
+    }
+
+}
